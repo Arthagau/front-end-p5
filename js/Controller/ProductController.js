05c3adbb4408cb661b79981fd5e7b9b15bd6060c
Maxime <hourmanma@gmail.com>
2021-07-06 16:46:59 +0200

    Ajout node

--- js/Controller/ProductController.js
+++ js/Controller/ProductController.js
@@ -1,15 +1,6 @@
 import {BASE_API_URL} from "../config.js"
 
-var ProductController = function ProductController(ProductView, ProductModel) {
-    this.ProductView = ProductView;
-    this.ProductModel = ProductModel;
-};
-
-let params = (new URL(document.location)).searchParams;
-let ProductId = params.get("id");
-
-async function getProduct() {
-   const response = await fetch(BASE_API_URL+`/furniture/${ProductId}`)
-   const data = await response.json();
-   return data
-}
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const idProduct = urlParams.get('id')
+console.log(idProduct);
